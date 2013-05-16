8dd4343d54c2199653ae3a274ddf021b79351c44
jeroenpeeters <jeroen@peetersweb.nl>
2013-05-16 11:15:32 +0200

    trying to get the slider to work

--- public/javascript/home-node.js
+++ public/javascript/home-node.js
@@ -1,29 +1,38 @@
 
 function AppliencesViewModel() {
   var self = this;
-  
+
   var staticRooms;
   self.rooms = ko.observableArray();
   self.selectedRoomId = ko.observable();
   //self.roomAppliences = ko.observable();
-  self.getRoomUrl = function(room) { 
-    return '#' + room; 
+  self.getRoomUrl = function(room) {
+    return '#' + room;
   };
-  
+
   self.sendOnCommand = function(appliance){
     socket.emit('device-on', appliance );
   };
-  
+
   self.sendOffCommand = function(appliance){
     socket.emit('device-off', appliance );
   };
-  
+
   self.data = ko.observableArray();
 
   self.availableAppliences = ko.observableArray();
-  
+
+  self.initSliders = function(){
+    alert('initsliders')
+    $('.sl').slider({
+      formater: function(value) {
+        return 'Current value: '+value;
+      }
+    });
+  }
+
   var initSammy = function(){
-    // Client-side routes    
+    // Client-side routes
     Sammy(function() {
       this.get('#:roomId', function() {
           self.selectedRoomId(this.params.roomId);
@@ -31,13 +40,13 @@ function AppliencesViewModel() {
           //self.chosenMailData(null);
           //$.get("/mail", { folder: this.params.folder }, self.chosenFolderData);
       });
-      
+
       //Default routes forwards to first room
       this.get('', function() { this.app.runRoute('get', '#' + staticRooms[0]) });
       console.log('initSammy: ' + staticRooms);
     }).run();
   }
-  
+
   var socket = io.connect('http://home.peetersweb.nl:8080');
   socket.on('devices', function (devices) {
     console.log(devices);
@@ -50,11 +59,11 @@ function AppliencesViewModel() {
     self.rooms(tmpRooms);
     initSammy();
   });
-  
+
   socket.on('device-status', function(device) {
     console.log('device-status: ' + device);
   });
- 
+
 }
 
 ko.applyBindings(new AppliencesViewModel());
