738b84077d74f968fb6a03724368fe142a6f90c8
jeroenpeeters <jeroen@peetersweb.nl>
2013-05-25 21:32:51 +0200

    created a basic standalone mochad simulator
    This simulator can be useful when debugging and when no mochad service
    is installed and/or no mochad compatible USB device is available when
    developing.

---
+++ mochad-simulator.js
@@ -0,0 +1,49 @@
+var net = require('net')
+    , config = require('./config.json')
+    
+var plCommandOnOffRegexp = /pl ([a-z][0-9]) (on|off)\r?\n?/
+
+function getDateString(){
+    var d = new Date()
+    return (d.getMonth()+1) + "/" + (d.getDate()>9?d.getDate():'0'+d.getDate()) + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds() + " ";
+}
+        
+function PLOnOff(match) {
+    var address = match[1].toUpperCase(), // ex: a1
+        command = match[2] // on|off
+    
+    return getDateString() +
+        "Tx PL HouseUnit: " + address + "\r\n"+
+        getDateString() + "Tx PL House: " + address.charAt(0) + " Func: " + command.substr(0,1).toUpperCase() + command.substr(1) + "\r\n";
+    
+//12/07 20:49:10 Tx PL HouseUnit: C3
+//12/07 20:49:10 Tx PL House: C Func: Off
+}
+
+
+net.createServer(function(sock){
+    
+    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort)
+    
+    sock.on('data', function(data){
+        var match = null;
+        
+        var response = '';
+        
+        if( ( match = plCommandOnOffRegexp.exec(data) ) != null){
+            response = PLOnOff(match)
+        }else{
+            console.log('Unsupported Mochad command | ' + sock.remoteAddress + ': ' + data)
+        }
+        
+        sock.write(response)
+        
+    })
+    
+    sock.on('close', function(data) {
+        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort)
+    })
+    
+}).listen(config.mochad_port, config.mochad_host)
+
+console.log('Mochad Simulator listening on ' + config.mochad_host +':'+ config.mochad_port)
